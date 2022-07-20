eb83e9ada5a1a2d4c1b0a1a72ff3aceb833b7286
Muhammad Gomaa <gomaam2711@gmail.com>
2022-07-20 22:59:41 +0200

    first commit

---
+++ Klickit_API/Models/DB_Context.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Klickit_API.Models
+{
+    public partial class DB_Context : DbContext
+    {
+        public DB_Context()
+            : base("name=DB_Context")
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.Product)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Orders)
+                .WithRequired(e => e.User)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
---
+++ Klickit_API/Models/Product.cs
@@ -0,0 +1,29 @@
+namespace Klickit_API.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [Key]
+        public long Prod_ID { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Prod_Name { get; set; }
+
+        public double Prod_Price { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
